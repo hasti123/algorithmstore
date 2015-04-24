9f05aad63794c1fc0229943a70766cdb438dfdaa
Charlie Hastings <chastings123@gmail.com>
2015-04-23 23:12:27 -0500

    made narrowdown function recursive

--- binarychoice.py
+++ binarychoice.py
@@ -1,4 +1,4 @@
-def runProgram():
+def run():
     print("Enter the number of values you want to choose from(e.g. 1,2...10)")
     listlength = int(input())
     choices = []
@@ -7,23 +7,23 @@ def runProgram():
         temp = input()
         choices.append(temp)
 
-    while(len(choices) > 1):
-        choices = narrowdown(choices)
-
-    print(choices)
+    print("Here is the winner! -> {0}".format(narrowdown(choices)))
 
 def narrowdown(choices):
     tempList = []
+
+    if len(choices) <= 1:
+        return choices[0]
 
     if len(choices) % 2 != 0:
           choices.append("") 
     
     for i in range(0,len(choices),2):
-        print("1: {0} or 2: {1}".format(choices[i],choices[i+1]))
+        print("Enter 1 for {0}\nEnter 2 for {1}".format(choices[i],choices[i+1]))
         choice = int(input())
         if choice == 1:
             tempList.append(choices[i])
         else:
             tempList.append(choices[i+1])
-        
-    return tempList
+    
+    return narrowdown(tempList)
